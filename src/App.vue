93b823ca633e8ba49fd0239f13e93fcb38477230
glbdmn <gabrieledamiani@hotmail.it>
2023-05-09 17:21:23 +0200

    fine milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,56 @@
 <script>
 
+import axios from 'axios'
+import SearchComp from './components/SearchComp.vue'
+import {store} from './store'
+
+
+export default {
+    name: "app",
+    components: {
+        SearchComp,
+    },  data(){
+            return{
+                store
+            }
+        },
+        created(){
+            this.chiamataApi()
+        },
+        methods: {
+            chiamataApi() {
+                // axios.get(`https://api.themoviedb.org/3/search/tv?api_key=697c85f36243aecef552ab548222820c&query=${ store.testoRicerca}`)
+                // .then((res)=> {
+                //    const datiApi = res.data.results
+                //     this.store.arrayDati = datiApi
+                // })
+                axios.get(`https://api.themoviedb.org/3/search/movie?api_key=697c85f36243aecef552ab548222820c&query=${ store.testoRicerca }`)
+                .then((res)=> {
+                    // console.log(res.data)
+
+                   const datiApi = res.data.results
+                    this.store.arrayDati = datiApi
+                    console.log(datiApi)
+                })
+            }
+        }
+}
+
 </script>
 
 <template>
-
+<SearchComp @nomeEmit="chiamataApi" class="container"/>
   
 </template>
 
 <style lang="scss">
 @use './style/main.scss';
 
+
 // richiesta api esempio movie
 // https://api.themoviedb.org/3/movie/550?api_key=697c85f36243aecef552ab548222820c
 
-
+// mia chiave api 
+// 697c85f36243aecef552ab548222820c
 
 </style>
---
+++ src/components/SearchComp.vue
@@ -0,0 +1,28 @@
+<script>
+import {store} from '../store'
+
+export default {
+    name: "SearchComp",
+    data(){
+        return {
+            store
+        }
+    }
+}
+</script>
+
+
+
+<template>
+    <div>
+        <input type="text" placeholder="cerca il tuo film" v-model="store.testoRicerca">
+        <button  @click="$emit('nomeEmit')"> ricerca </button>
+        <span v-for="(elem, index) in store.arrayDati"
+        :key="index"> <br><strong>{{ elem.title }}</strong>  <br> {{ elem.original_title }} <br> {{ elem.original_language }} <br> {{ elem.vote_average }}</span>
+    </div>
+</template>
+
+
+<style>
+
+</style>
