f18095d5fb63bf403d6d159214c2c3fd87f0eba4
glbdmn <gabrieledamiani@hotmail.it>
2023-05-09 12:26:53 +0200

    completata milestone 0

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,11 @@
 </template>
 
 <style lang="scss">
-@use '/main.scss';
+@use './style/main.scss';
+
+// richiesta api esempio movie
+// https://api.themoviedb.org/3/movie/550?api_key=697c85f36243aecef552ab548222820c
+
+
 
 </style>
