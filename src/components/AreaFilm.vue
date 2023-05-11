11ec868f89db3132ff9e695afee89492c1bbb2d6
glbdmn <gabrieledamiani@hotmail.it>
2023-05-11 12:59:48 +0200

    aggiunta voto e stelline

--- src/components/AreaFilm.vue
+++ src/components/AreaFilm.vue
@@ -17,13 +17,13 @@ export default {
 
 <template>
     <div class="row" v-if="store.arrayFilm.length">
-        <h2>film</h2>
+        <h2 class="p-2">Film</h2>
         <SingolaCard v-for="(film, index) in store.arrayFilm"
         :key="index"
         :info="film" />
     </div>
     <div class="row" v-if="store.arraySerieTv.length">
-        <h2>serie</h2>
+        <h2 class="p-2">Serie Tv</h2>
         <SingolaCard v-for="(serie, index) in store.arraySerieTv"
         :key="index"
         :info="serie"/>
--- src/components/SingolaCard.vue
+++ src/components/SingolaCard.vue
@@ -22,6 +22,16 @@
             } else {
                 return this.info.original_name
             }
+         },
+         images(){
+            if( this.info.poster_path){
+                return `https://image.tmdb.org/t/p/w500/${this.info.poster_path}`
+            } else if( this.info.poster_path === null )  {
+                return '/images/virus-img-01.jpg'
+            }
+         },
+         votoStelline(){
+            return Math.ceil(this.info.vote_average / 2) 
          }
      },
     data(){
@@ -34,29 +44,16 @@
 
 <template>
      <div class="col-3">
-        <div class="card position-relative mb-2">
-             <img :src= "`https://image.tmdb.org/t/p/w342/${info.poster_path}`" class="card-img-top" alt="">
+        <div class="card position-relative mb-3">
+             <img :src= " images() " class="card-img-top" alt="">
             <div class="card-body position-absolute bg-white 2 ">
              <h5 class="card-title"> {{ title() }} </h5>
              <p class="">{{ info.original_title }}</p>
              <p>{{ info.title }}</p>
              <p>{{ info.original_language}} <img width="20" :src="bandiereStampate(info.original_language)" alt=""> </p>
-             <p>{{ info.vote_average }} <i class="fa-regular fa-star" style="color: #000000;"></i> </p>
+             <p> voto: {{ votoStelline() }} <i v-for="n in 5" class="fa-star" :class=" ( n <= votoStelline() ) ? 'fa-solid' : 'fa-regular' " ></i> </p>
             </div>
         </div>
 
     </div>
-      <!-- <div class="col-3">
-        <div class="card">
-             <img src="" class="card-img-top" alt="...">
-            <div class="card-body">
-             <h5 class="card-title"> {{ infoSerieTv.name }} </h5>
-             <p class="">{{ infoSerieTv.name }}</p>
-             <p>{{ infoSerieTv.name }}</p>
-             <p>{{ infoSerieTv.language }}</p>
-             <p>{{ infoSerieTv.vote_average }}</p>
-            </div>
-        </div>
-
-    </div>   -->
 </template>
